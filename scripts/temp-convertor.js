612a96e7bb5619b86365660ebb689f4409786572
Zy'Aire McCreary-Scott <zyaire.mccrearyscott@gmail.com>
2022-10-21 14:41:40 -0400

    added a css page and styles

--- scripts/temp-convertor.js
+++ scripts/temp-convertor.js
@@ -9,15 +9,19 @@ function init(){
 function onConvertBtnClicked(){
     console.log("hey");
 
+
     let celsFeield= document.getElementById("celsField");
     let cels= Number(celsFeield.value);
-    let fahrenhiet=(cels*9/5)+32;
+    let fahrenFeield= document.getElementById("fahrenField");
+    let fahren= Number(fahrenFeield.value);
+    let fahrenhiet=(cels-9/5)+32;
+    fahrenFeield.value= fahrenhiet;
     console.log(fahrenhiet);
 
-    let fahrenFeield= document.getElementById("fahrenField");
-    let fahren= Number(celsFeield.value);
+    // let fahrenFeield= document.getElementById("fahrenField");
+    // let fahren= Number(celsFeield.value);
     let celsius=(fahren-32)*5/9;
-    console.log(celsius);
+    // console.log(celsius);
 // after getting the number to show in the textbox put thie righ codea into their html sheet 
     
 }
