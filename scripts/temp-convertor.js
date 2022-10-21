f8b407df94f5a6e733e37b554f819970532c0b5a
Zy'Aire McCreary-Scott <zyaire.mccrearyscott@gmail.com>
2022-10-21 10:19:09 -0400

    add click behave on button and the function when its clicked

---
+++ scripts/temp-convertor.js
@@ -0,0 +1,23 @@
+window.onload= init();
+
+function init(){
+    let convertBtn= document.getElementById("convertBtn");
+
+    convertBtn.addEventListener("click", onConvertBtnClicked);
+}
+
+function onConvertBtnClicked(){
+    console.log("hey");
+
+    let celsFeield= document.getElementById("celsField");
+    let cels= Number(celsFeield.value);
+    let fahrenhiet=(cels*9/5)+32;
+    console.log(fahrenhiet);
+
+    let fahrenFeield= document.getElementById("fahrenField");
+    let fahren= Number(celsFeield.value);
+    let celsius=(fahren-32)*5/9;
+    console.log(celsius);
+// after getting the number to show in the textbox put thie righ codea into their html sheet 
+    
+}
