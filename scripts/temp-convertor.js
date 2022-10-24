7dc808774e7b5b11dacb87eca64cbb27fa972f04
Zy'Aire McCreary-Scott <zyaire.mccrearyscott@gmail.com>
2022-10-24 08:45:13 -0400

    try to add my js  in the js file

--- scripts/temp-convertor.js
+++ scripts/temp-convertor.js
@@ -1,27 +1,33 @@
-window.onload= init();
+window.onload = init();
 
-function init(){
-    let convertBtn= document.getElementById("convertBtn");
+function init() {
+    let convertBtn = document.getElementById("convertBtn");
 
     convertBtn.addEventListener("click", onConvertBtnClicked);
 }
 
-function onConvertBtnClicked(){
+function onConvertBtnClicked() {
     console.log("hey");
 
+    let celsFieeld = document.getElementById("celsField");
+    let cels = Number(celsFieeld.value);
+    let fahrenField = document.getElementById("fahrenField");
+    let fahren = Number(fahrenField.value);
 
-    let celsFeield= document.getElementById("celsField");
-    let cels= Number(celsFeield.value);
-    let fahrenFeield= document.getElementById("fahrenField");
-    let fahren= Number(fahrenFeield.value);
-    let fahrenhiet=(cels-9/5)+32;
-    fahrenFeield.value= fahrenhiet;
-    console.log(fahrenhiet);
+    if(fahrenField===Number){
+        let fahrenheit = Math.ceil((cels*9/5)+32);
+        fahrenField.value = `${fahrenheit} degrees`;
+    } 
+    else(celsField===Number){
+        let celsius=Math.ceil((cels*9/5)+32);
+        celsField.value = celsius;
+    }
 
-    // let fahrenFeield= document.getElementById("fahrenField");
-    // let fahren= Number(celsFeield.value);
-    let celsius=(fahren-32)*5/9;
-    // console.log(celsius);
-// after getting the number to show in the textbox put thie righ codea into their html sheet 
-    
+               // error message if the input is a NAN
+  let error= document.getElementById("error");
+  if (isNAN(fahren)){
+      error.innerText= "Only use numbers";
+  }
+
 }
+ 
